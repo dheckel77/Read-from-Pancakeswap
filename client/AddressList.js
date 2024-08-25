932fbddcbd1d6366104b2aa98a40a790c42e96ef
dheckel77 <dheckel77@gmail.com>
2024-08-25 07:51:07 -0400

    Updated address list

--- client/AddressList.js
+++ client/AddressList.js
@@ -1,5 +1,5 @@
-const addressFactory = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
-const addressRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const addressFactory = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"; // Pancakeswap 
+const addressRouter = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // Pancakeswap
 const addressFrom = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"; // BUSD
 const addressTo = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"; // WBNB
 
